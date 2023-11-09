5aebe8fec3c36bf69bd1fedaf8ab106bb7d3e00d
Carlos R. Mercado <carlos.mercado@flipsidecrypto.com>
2023-11-09 13:01:52 -0500

    for simplicity just leak the json worker for now....
    environments not importing correctly

--- src/utils/DataProcess.js
+++ src/utils/DataProcess.js
@@ -22,7 +22,7 @@ export function uppercaseKeys(data) {
 // reads from local 
 // const TX_STATS_PATH = 'test-data/ecosystem_17_cards.json';
 // production 
-const TX_STATS_PATH = env.TX_STATS_PATH;
+const TX_STATS_PATH = "http://ethmetrics.carlos-mercado.workers.dev";
 
 // Fetch function to load the JSON file
 export async function fetchData(path) {
